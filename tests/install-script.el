1a837be6f71b506eee02b4303bdf8fe8f82dec41
okamsn <okamsn@users.noreply.github.com>
2025-01-13 21:25:47 -0500

    Update GitHub Action for re-organized repo for base-package.
    File were moved around in the Loopy repository.

--- tests/install-script.el
+++ tests/install-script.el
@@ -1,8 +1,15 @@
+;; -*- lexical-binding: t; -*-
 (setq package-user-dir "~/.emacs.d/elpa")
 (message "Current directory: %s" default-directory)
 (require 'package)
 (package-refresh-contents)
-(message "\nInstall main package from directory:\n")
-(package-install-file (expand-file-name "loopy/loopy/" default-directory))
+
+(message "\nInstall Loopy from tar file:")
+(let ((tar-files (directory-files (expand-file-name "loopy/" default-directory)
+                                  t
+                                  "\\`loopy-.*?.tar\\'")))
+  (cl-assert (= 1 (length tar-files)))
+  (package-install-file (cl-first tar-files)))
+
 (message "\nInstall from file:\n")
 (package-install-file (expand-file-name "loopy-dash.el" default-directory))
